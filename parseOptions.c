60915fdcede3437e197e5a0fe077f387b0e0df15
Charlie Miller <charliebobmiller@gmail.com>
2022-09-27 14:41:38 -0500

    began working on methods to parse options

---
+++ parseOptions.c
@@ -0,0 +1,15 @@
+///////////includes
+#ifndef stdio
+    #include "stdio.h" 
+    #endif
+#ifndef string
+    #include "string.h" 
+    #endif
+#ifndef stdlib
+    #include "stdlib.h"
+    #endif
+#ifndef stringArray
+    #include "stringArray.c"
+    #endif
+
+
